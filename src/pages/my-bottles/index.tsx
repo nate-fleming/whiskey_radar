cb2bc9889a3bd061ce5e47865306a565ae18c347
nate-fleming <nfleming4@gmail.com>
2020-07-09 22:59:48 -0500

    add react select and mock data for bottles

--- src/pages/my-bottles/index.tsx
+++ src/pages/my-bottles/index.tsx
@@ -1,38 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import { getUserBottles } from '../../api/bottles'
-import { Bottle } from '../../api/bottles/mocks';
-import Loader from '../../components/loaders/radarLoader'
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import { getUserBottles } from "../../api/bottles";
+import { Bottle } from "../../api/bottles/mocks";
+import Loader from "../../components/loaders/radarLoader";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { mockBottlesResponse } from "../../api/bottles/mocks/mockBottles";
 
-const MyBottles:React.FC = () => {
-    const [bottles, setBottles] = useState<Bottle[]>([]);
-    const [isLoading, setLoading] = useState(true);
+const MyBottles: React.FC = () => {
+  const [bottles, setBottles] = useState<Bottle[]>([]);
+  const [bottleOptions, setBottleOptions] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const userBottles = setTimeout(() => {
-            setBottles(getUserBottles);
-            setLoading(false);
-          }, 4000);
-          return () => clearTimeout(userBottles);
-    }, [])
+  useEffect(() => {
+    const userBottles = setTimeout(() => {
+      let bottleOptions: any = [];
+      mockBottlesResponse.forEach((bottle) =>
+        bottleOptions.push({ value: bottle.id, label: bottle.title })
+      );
+      setBottles(getUserBottles);
+      setBottleOptions(bottleOptions);
+      setLoading(false);
+    }, 4000);
+    return () => clearTimeout(userBottles);
+  }, []);
 
-    return (
-        <>
-        {
-            isLoading ? <Loader /> 
-            : 
-            <Bottles>
-                {bottles.map(bottle => 
-                <Bot key={bottle.id}>
-                    <Name>{bottle.name}</Name>
-                    <DeleteIcon className="fas fa-trash-alt"/>
-                </Bot>)}
-            </Bottles>
-        }
-        </>
-    )
-}
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Bottles>
+          <Select options={bottleOptions} />
+          {bottles.map((bottle) => (
+            <Bot key={bottle.id}>
+              <Name>{bottle.name}</Name>
+              <DeleteIcon className="fas fa-trash-alt" />
+            </Bot>
+          ))}
+        </Bottles>
+      )}
+    </>
+  );
+};
 
 export default MyBottles;
 
@@ -46,18 +56,18 @@ const Bottles = styled(motion.div)`
 `;
 
 const Bot = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin: 8px 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 8px 12px;
 `;
 
 const Name = styled.div`
-    color: white;
-    font-size: 16px;
-`
+  color: white;
+  font-size: 16px;
+`;
 
 const DeleteIcon = styled.div`
-    width: auto;
-    color: red
-`
+  width: auto;
+  color: red;
+`;
